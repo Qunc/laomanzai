4154a27934ce80a1c9d0823bbe2b1b78ae746216
Thrall <thrall.chen@shangcars.com>
2016-11-08 20:48:45 +0800

    build config change

--- webpack.config.js
+++ webpack.config.js
@@ -60,11 +60,13 @@ if (process.env.NODE_ENV === 'production') {
         NODE_ENV: '"production"'
       }
     }),
+      /*
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
       }
     }),
+    */
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
