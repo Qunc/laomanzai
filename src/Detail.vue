3bcc2c3808227ce86d5f69765979987995f7b59d
Thrall <thrall.chen@shangcars.com>
2016-11-08 22:13:04 +0800

    build options

--- src/Detail.vue
+++ src/Detail.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <header class="header_title">
-            <img src="./assets/img/left.png"/><span>订单详情</span>
+            <img src="./assets/img/left.png" onclick="window.history.go(-1)"/><span>订单详情</span>
         </header>
         <section>
             <div class="details">
@@ -50,7 +50,6 @@ module.exports = {
     },
     created: function () {
         this.fetchData();
-
     },
     methods: {
         fetchData: function () {
